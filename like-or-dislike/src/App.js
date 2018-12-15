92216be38120020e0695eb35a5f986dd9a36581c
hjbc0921 <hjbc0921@gmail.com>
2018-12-16 01:33:34 +0900

    top level component with state

--- like-or-dislike/src/App.js
+++ like-or-dislike/src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LikeOrDislike from './LikeOrDislike'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      author: "Hyunji Choi",
+      questions: [
+        {
+          subject: "아이스 바닐라 라떼",
+          choice: "like"
+        },
+        {
+          subject: "밀크티",
+          choice: "like"
+        },
+        {
+          subject: "녹차 아이스크림",
+          choice: "like"
+        },
+        {
+          subject: "초밥",
+          choice: "like"
+        },
+        {
+          subject: "고추장불고기",
+          choice: "dislike"
+        },
+        {
+          subject: "김치찌개",
+          choice: "dislike"
+        },
+        {
+          subject: "선지국",
+          choice: "dislike"
+        },
+        {
+          subject: "소주",
+          choice: "dislike"
+        }
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <LikeOrDislike {...this.state} />
       </div>
     );
   }
