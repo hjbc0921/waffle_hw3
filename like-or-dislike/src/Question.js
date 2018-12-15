ec0938d8d82f453a8b58c4802a70c3b300fddb89
hjbc0921 <hjbc0921@gmail.com>
2018-12-16 01:52:21 +0900

    Question component

---
+++ like-or-dislike/src/Question.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Button, ButtonGroup } from 'react-bootstrap';
+
+class GetName extends Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  render() {
+    return (
+      <div>
+      <h2 style={{margin:"0 0 20px 0"}}>{this.props.food}?</h2>
+      <div style={{maxWidth:"80%",margin:"auto"}}>      
+        <Button 
+        bsStyle="success" 
+        bsSize="large"
+        type="submit" 
+        block
+        style={{margin:"20px 0 0 0"}} 
+        onClick={() => this.props.saveAnswer("like")} 
+        >호</Button>
+        <Button 
+        bsStyle="danger" 
+        bsSize="large"
+        type="submit" 
+        block
+        style={{margin:"20px 0 0 0"}} 
+        onClick={() => this.props.saveAnswer("dislike")}  
+        >불호</Button>
+      </div>
+      </div>
+    );
+  }
+}
+
+
+export default GetName
